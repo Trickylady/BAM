f98d456b57b64e8e5894e6f11f9b1cc2bd44af76
Trickylady <evi.muselle@gmail.com>
2025-11-21 06:15:56 +0100

    menu update

--- menu.gd
+++ menu.gd
@@ -1,5 +1,23 @@
-extends Node2D
+extends Control
 
 
-func _on_b_about_button_down() -> void:
-	pass # Replace with function body.
+# buttons colors
+var hover_color := "#4b294b"
+var pressed_color := "#4b6d5f"
+
+
+func _ready():
+	for btn: Button in get_tree().get_nodes_in_group("buttons"):
+		if not btn.get_child_count() == 1:
+			continue
+		if not btn.get_child(0) is RichTextLabel:
+			continue
+		
+		var label: RichTextLabel = btn.get_child(0)
+		btn.pressed.connect( update_label_color.bind(pressed_color, label, label.text) )
+		btn.mouse_entered.connect( update_label_color.bind(hover_color, label, label.text) )
+		btn.mouse_exited.connect( update_label_color.bind("white", label, label.text) )
+
+
+func update_label_color(color: String, label: RichTextLabel, text: String) -> void:
+	label.text = "[color=%s][wave amp=20 freq=7]%s[/wave][/color]" % [color, text]
