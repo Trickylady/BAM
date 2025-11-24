ce03b46015c6f53c596500224667f837e856bd80
Trickylady <evi.muselle@gmail.com>
2025-11-24 05:41:13 +0100

    stuff I forgot to turn off and on

--- player.gd
+++ player.gd
@@ -39,8 +39,8 @@ func _physics_process(delta):
 	
 	if not moving:
 		var input = Vector2(
-			Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-			Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+			Input.get_action_strength("right") - Input.get_action_strength("left"),
+			Input.get_action_strength("down") - Input.get_action_strength("up")
 		)
 
 		if input != Vector2.ZERO:
